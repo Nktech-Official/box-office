02020858a4f6c2d09da440c4a34440966c23779b
nktech-official <76529189+Nktech-Official@users.noreply.github.com>
2023-06-12 13:38:17 +0530

    coustom hook to fetch data from api

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,5 +1,37 @@
 import { useParams } from 'react-router-dom';
+import { getShowById } from '../api/tvmaze';
+import { useEffect, useState } from 'react';
+
+const useShowById = showId => {
+  const [showData, setShowData] = useState(null);
+  const [showError, setShowError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setShowError(null);
+        const data = await getShowById(showId);
+        setShowData(data);
+      } catch (error) {
+        console.log(error);
+        setShowError(error);
+        setShowData(null);
+      }
+    }
+    fetchData();
+  }, [showId]);
+  return { showData, showError };
+};
+
 export default function Show() {
   const { showId } = useParams();
-  return <div>{showId}</div>;
+  const { showData, showError } = useShowById(showId);
+
+  if (showError) {
+    return <div>We have an Error : {showError.message}</div>;
+  }
+  if (showData) {
+    return <div>Got show Data : {showData.name}</div>;
+  }
+  return <div>Data is Loading</div>;
 }
